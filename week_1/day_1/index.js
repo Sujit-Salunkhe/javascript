b7923ef51fe0b73ebbd1daaa1329b6acbeeb5d7f
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-07 19:00:36 +0530

    learning if else, operaters

---
+++ week_1/day_1/index.js
@@ -0,0 +1,10 @@
+ 
+    // ways to print in javascript
+    // alert("me")
+    // document.write("this is document write")
+    // console api
+
+    // console.log("hello world");
+    // console.warn("warning is now warn")
+    // console.error("error is error") 
+    console.log("hello git")
---
+++ week_1/day_2/index.js
@@ -0,0 +1,3 @@
+var number1 = 50
+var number2 =50
+console.log(number1+number2);
