ee371f47f952be5f9da269c20f690530e8f1ce7f
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-20 17:44:06 +0530

    learning java script condition operators type of

---
+++ week_1/week_2/day_1/practise.js
@@ -0,0 +1,23 @@
+// alert("sujit")
+// let good="jock"
+// let bad="bad"
+// console.log(bad+ good)
+
+let name='john';
+let admin=name;
+// alert(admin);
+let sujit="sujit salunkhe " 
+// sujit=4950
+// console.log(sujit)
+// console.log(1/0)
+// alert(Infinity)
+// alert("sujit"*34)
+let bignmbers=12525625181541254814n;
+console.log(bignmbers)
+console.log(`my name is ${sujit}`)
+console.log(`${1 + 45}`)
+let check=4<89;
+// console.log(check)
+// typeof null
+console.log(typeof null)
+console.log(typeof name)
