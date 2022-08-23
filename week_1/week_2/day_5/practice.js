8490440843326bb40e2de8c8397ce3b4c1ddd2a4
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-23 17:12:30 +0530

    learning functions and loops , and operaters

---
+++ week_1/week_2/day_5/practice.js
@@ -0,0 +1,45 @@
+// function message(){
+//     alert("show a message")
+// }
+// message()
+// message()
+// message()
+// // message()
+// let user ="sujit"
+// function username(){
+//      user="vaishnav"
+//     let message="I am most populaer user of java script " + user
+//     alert(message)
+// }
+// alert(user)
+// // username()
+// // // alert(user)
+// function message (name,text="you are good at coding"){
+//     // name="*"+name+"*"
+//     alert(name+":"+ text)
+// }
+// // let a="suraj said"
+// // message("sujit is dashong boy")
+// function test(name,text=message(name)){
+//     alert(name + text)
+    
+
+// }
+// // test("sujit","I am good at everything")
+// test("sujit")
+// // test("sujit")
+// // // alert("sujit")
+// function check_age(n){
+// return (n>18)? true:confirm("did your parants  allow you")
+// }
+// check_age(5)
+// function smallnumber(a,b){
+//    return (a<=b) ? a:(a>=b)? b :a;
+// alert(smallnumber(a,b)) \
+// function power(x,n){
+    // return x ** n
+// }
+
+// let a=+prompt("type a number")
+// let b=+prompt("type a 2 number")
+// alert(power(a,b))   
