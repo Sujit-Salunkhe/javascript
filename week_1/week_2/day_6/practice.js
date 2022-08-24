75f13c71740845dd4fb83de7684cc14519ffbd03
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-24 18:07:37 +0530

    learninig functions expressions

---
+++ week_1/week_2/day_6/practice.js
@@ -0,0 +1,34 @@
+// //  learning functions expressions
+// function sayHi() {
+//     alert( "Hello" );
+//   }
+  
+//   alert( sayHi ); 
+//   let func=sayHi;
+//   func()
+//   sayHi()
+//   let sayHi = function() { // (1) create
+//     alert( "Hello" );
+// //   };
+  
+// // sayHi()
+// function ask(quation,yes,no){
+//     if (confirm(quation)) {
+//         yes()}
+//     else no()
+// }
+// function showok(){
+//     alert("you agreed ");
+// }
+// function showcancel(){
+//     alert("you cancled the execution.");
+// }
+// ask("why you are here",
+// function()  {alert("you agreed for what purpose");},
+// function(){alert("you cancelled the execution why?, you want to continue.")})
+let ask=function(){
+    alert("this is an function expression")
+
+
+}
+// alert(4 && 0);
