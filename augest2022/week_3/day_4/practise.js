c80fa8b5974702aa929f8f66e63b050b8b8f5751
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-01 19:53:31 +0530

    Learning this function of object and reference of object ,garbage collection and it's reference

---
+++ augest2022/week_3/day_4/practise.js
@@ -0,0 +1,43 @@
+// let user = {
+//     name:"sujit"
+// };
+// // let admin=user;
+// // alert(user.name)
+// // admin.name="salunkhe"
+// // alert(user.name)
+// let clown ={};
+// for(key in user){
+//     clown[key]=user[key]
+// }
+// clown.name="Vaishnav"
+// alert(clown.name)
+// user.name="Salunkhe"
+// alert(user.name)
+// let user={
+//     name :"sujit"
+// }
+// let lastname ={
+//     lname:"salunkhe"
+// }
+// let middlename ={
+//     mname:"Yuvraj"
+// }
+// Object.assign(user,lastname,middlename)
+// alert(user.lname)
+// let clown= Object.assign({},user)
+// alert(clown.name)
+// let user ={
+//     name:"sujit",
+//     area:{
+//         height:1,
+//         width:2,
+//     }
+// };
+// let clown=Object.assign({},user)
+// clown.area.height=89;
+// alert(clown.area.height)
+// let clown=structuredClone(user);
+// alert(clown.area.height===clown.area.height)
+// user.area.height=546
+// alert(user.area.height)
+// alert(clown.area.height)
