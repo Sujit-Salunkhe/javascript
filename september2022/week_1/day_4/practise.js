e0f2748dd5873b0dc97fd84a08d733e6199bf8fc
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-06 17:07:16 +0530

    learnig objects constructing and symbol type

---
+++ september2022/week_1/day_4/practise.js
@@ -0,0 +1,37 @@
+// let id=Symbol("id");
+// let id2=Symbol("id");
+// alert(id == id2);
+// // id ="Id is  the id this is the unique identifier"
+// alert(id.description);
+// let id=Symbol("id")
+// let symbol=Symbol("Second symbol")
+// let user={
+//     name:"sujit",
+//     age:18,
+//     [id]:"sujit Is the king of the world",   
+//     [symbol]:"Who are you I am real symbol"
+// }
+// for(key in user) alert(key);
+// alert("king:"  +  user[id]);
+// user[id]="Sujit is great king"
+// // user[symbol]="I am a symbol"
+// // user[symbol]="I am a also symbol"
+// alert(user[symbol])
+// alert(user[id])
+// let id=Symbol("id");
+// let vaishnav=Symbol("V/aishnav")
+// user[id]='Their id  value'
+// user[id]='git  and git hub'
+// user[vaishnav]="sujit";
+// alert(user[vaishnav])
+// alert(user[id]);
+// alert(id.toString())
+// alert(id.description)
+// user.[id]="sujit"/   
+// alert(user[id])
+// let id =Symbol("id")
+// let user={
+//     [id]:"It's an id "
+// };
+// let copy=Object.assign({},user)
+// alert(copy[id])
