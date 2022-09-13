d0d0a833929d51679bad21025679fc7f8adbc3af
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-13 15:51:18 +0530

    learning primitive metheds and numberes in detailed

---
+++ september2022/week_2/day_2/practise.js
@@ -0,0 +1,35 @@
+// let str="sujit"
+// alert(str.toUpperCase());
+// let number=5.89787654
+// alert(number.toFixed(5))
+// alert (typeof 0)
+// alert(typeof new Number(0))
+// let zero=new Number(0)
+// // zero=0
+// if (zero){
+//     alert("zero is a greater Number")
+// }
+// let str = "Hello";
+
+// str.test = 5; // (*)
+
+// alert(str.test);
+let billions =1e9;
+alert(billions)
+alert(1e3 === 1 * 1000)
+alert(85e-5)
+// -3 divides by 1 with 3 zeroes
+1e-3 === 1 / 1000; // 0.001
+
+// -6 divides by 1 with 6 zeroes
+1.23e-6 === 1.23 / 1000000; // 0.00000123
+
+// an example with a bigger number
+1234e-2 === 1234 / 100; // 12.34, decimal point moves 2 times
+let mcs = 1e-6; // five zeroes to the left from 1
+let mсs = 0.000001;
+1e3 === 1 * 1000; // e3 means *1000
+1.23e6 === 1.23 * 1000000; // e6 means *1000000'
+let billion = 1e9;  // 1 billion, literally: 1 and 9 zeroes
+
+alert( 7.3e9 );  //
