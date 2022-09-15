f9203a32ddeeb92a167446c1abe2306317285320
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-15 21:29:11 +0530

    learanin various methods of string

--- september2022/week_2/day_3/practise.js
+++ september2022/week_2/day_3/practise.js
@@ -1,2 +1,14 @@
-alert(5 ** 5)
-alert(9 / 2)
+// alert(5 ** 5)
+// alert(9 / 2)
+// function myfunction(a,b){
+//     return a * b
+// }
+// alert(myfunction(5,4))
+// document.getElementById('time').style.background='black'
+function displaydate(){
+    document.getElementById('time').innerHTML=Date();
+}
+prasad
+sujit
+prasad
+vaishnav
---
+++ september2022/week_2/day_4/practise.js
@@ -0,0 +1,18 @@
+// function addhtml(){
+// document.getElementById('events').innerHTML='sujit is the most sincer boy of teh world , why my mom is watching all type of drama'
+// };
+// let text="sujit is the greatest boy ever , He is most talented boy has ever seen by me";
+// alert(text.length);
+// alert("We are the kings And \"sujit\" is the our king")
+// alert("it\'s")
+// let sujit='sujit \
+// salunkhe'
+// let x="Salunkhe";
+// let y=new String("Salunkhe");
+// let s=new String("Yuvraj Salunkhe");
+// let p=new String("Yuvraj Salunkhe");
+// alert(typeof x + " " +typeof y);
+// alert(typeof s);
+// alert(s == p);
+let Name="Sujit Yuvraj Salunkhe"
+// alert(Name.substr(0,0);)
