fd20acd57e5d62623bb4bc4c8b990a2519eff661
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-27 21:49:04 +0530

    practising string methods

---
+++ september2022/week_3/day_2/practise.js
@@ -0,0 +1,13 @@
+// alert("Sujit is the best boy eve/..r in the world");
+// alert("India is a very strong country esspcislly when war it's war start against ukraine, Why you're so cute siddesha mother fucker sujit man")
+// let car = "xuv700";
+// let car2 ="Mahindra Thar";
+// alert(car.length);
+// alert(car2.length);
+// alert("It/'s a cake a beutiful cake from beutiful palce man");
+// alert("It's not good to say false boutsomething man ")
+let fname="Sujit Yuvraj Salunkhe";
+alert(fname.substring(6,12));
+alert("Sub chutiyapa hai")
+
+// alert(fname.indexOf("Y"))
