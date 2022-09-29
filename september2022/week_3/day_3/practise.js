19c5ea948ddb024b982cfa7b8c7159c719a071ff
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-29 22:52:56 +0530

    learning strings

---
+++ september2022/week_3/day_3/practise.js
@@ -0,0 +1,22 @@
+// alert("I know a lot of string's methods but My target is to study unforgatabel man. I study further like financial management,cost accouting")
+// let text="This charter is called backslash ";
+// // alert(text);;
+// let x="Sujit Yuvraj Salunkhe";
+// let y= new String("Sujit Yuvraj Salunkhe");
+// // alert(typeof y);
+// // alert(typeof x);
+// // alert(x === y);
+// let t = new String("John cena");
+// let u = new String("John cena")
+// // alert(x == y);
+// alert(t == u);
+// let fname="Why love is most powerfull in a world why When love is called most powerfull weapoen in a world. I fill it today, Why people are dyed for there love I understand that today I can't say that because I built different so what happend to me really I don't know what happende to me why man it's happed to me man just have coloser look to thah";
+// console.log(fname.indexOf("love"));
+// let y=`Why you loved you me so much I don't know is there is some other in your life let me know i don't want be a last person in your life why you hate me so much I don't know man just fuck off from my life.Songs are best mental exercise to remove anxity. Kuch bhi likhe ha ky we can't use in appororiate words here man`
+// let firstname="Sujit "
+// let middlenaem="Yuvraj"
+// let Lastname=" Salunkhe"
+// alert(firstname + middlenaem + Lastname)
+// let number=12.50;
+// alert(number.toFixed(1))
+// alert()
