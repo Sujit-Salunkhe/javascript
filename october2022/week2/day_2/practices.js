4acb037f37a1fef5bb0b641f8f388aa964524eb5
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-11-01 18:58:18 +0530

    leraning javascript various methods:

---
+++ october2022/week2/day_2/practices.js
@@ -0,0 +1,17 @@
+// alert("hello, sujit")
+// let home=["ghar","padoso","html"]
+// delete home[0]
+// alert(home[1])
+// alert(home.length)
+// alert(1 + 2)
+// alert("sujit is the mosts dumbest person as i ever seen ")
+// let name="Sujit";
+// console.log(name)
+// name =78
+// console.log(name)
+var a =45
+var a ="sujit"
+// let b="sujit"/
+// let b="sfuj"
+alert(a)
+const sujit ="topper"
