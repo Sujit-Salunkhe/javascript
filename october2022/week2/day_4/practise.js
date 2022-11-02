496e64731f623272041f5b62f894ef37af83bcc7
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-11-02 16:11:36 +0530

    learinng exprssions

---
+++ october2022/week2/day_4/practise.js
@@ -0,0 +1,16 @@
+// // alert("sujit is a smart boy")
+// let a="sujit"
+// let b=45
+// alert(a + b)
+// alert(typeof (a+b))
+// const student= {
+//     name:"sujit",
+//     game:"cricket",
+//     pokemon:"pikshow"
+// }
+// student["lname"]="Salunkhe"
+// student["marks"]="topper of the class"
+// console.log(student)
+// alert(student.name)
+// alert(student.pokemon)
+// alert(student.game)
