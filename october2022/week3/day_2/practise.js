4b9c41adf3e8997e26a26664b591899f5dd90dbd
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-11-04 14:43:18 +0530

    learning while loos of javascript

---
+++ october2022/week3/day_2/practise.js
@@ -0,0 +1,24 @@
+// alert("sujit is a mcu fan. But not that great")
+// for(let i =0;i<=100;i++){
+//     console.log(i)
+/*
+let sum=0
+let n= +prompt("what is your number")
+for(let i=0;i<n;i++){
+    sum += (i +1)
+    
+}
+alert(sum)
+// */
+// let marks ={
+//     sujit:99,
+//     ganesh:78,
+//     sawnat:51,
+//     ankit:4
+// }
+// for(let x  in marks ){
+//     alert("the marks of " + x + " are " + marks[x])
+// }
+// for(let x of "harry"){
+    // alert(x)
+// }
