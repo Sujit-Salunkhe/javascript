7de58f7807452f2f38a9ac2793ebe92152336b5f
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-07-30 17:46:46 +0530

    learning js fundamentals

---
+++ july2023/week_4/day_6/everything.js
@@ -0,0 +1,26 @@
+//     // ["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+//     //     alert(`${item} is at index ${index} in ${array}`);
+//     //   });
+
+// let sujit=["powerful","ambitious","Handsome"]
+// sujit.forEach((item) => {
+//     console.log(item)
+// })
+
+
+// sujit.map((value)=>{
+//     console.log(value)
+// })
+// values=[23,34,34,32,3,21,1]
+// let s=values.filter((a)=>{
+//     return a<10
+// })
+// console.log(s)
+
+sj=[1,2,3,4]
+cal=(h1,h2)=>{
+    return h1 + h2
+}
+let sn=sj.reduce(cal)
+
+console.log(sn)
