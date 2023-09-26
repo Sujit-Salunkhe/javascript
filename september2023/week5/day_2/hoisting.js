0b6534a27baf9ba4712b5b1b23433ef68ffed619
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-09-26 12:35:57 +0530

    lerning setTimeout,setINterval

--- september2023/week5/day_2/hoisting.js
+++ september2023/week5/day_2/hoisting.js
@@ -1,7 +1,31 @@
-sujit()
-function sujit (){
-    console.log("sujit is king")
-}
-let sayHi = new Function('alert("Hello")');
+// // sujit()
+// // function sujit (){
+// //     console.log("sujit is king")
+// // }
+// // let sayHi = new Function('alert("Hello")');
 
-sayHi(); 
+// // sayHi(); 
+
+
+
+//     let x=9;
+
+//     function see() {
+//         let x=7;
+//         let sujit = new Function( 'alert(x)'); 
+//         sujit()
+        
+//     }
+//     see()
+    // function getFunc() {
+    //     let value = "test";
+      
+    //     let func = new Function('alert(value)');
+      
+    //     return func;
+    //   }
+    let time=Date.now()
+    console.log(time)
+      
+    //   getFunc()(); 
+    // see()
