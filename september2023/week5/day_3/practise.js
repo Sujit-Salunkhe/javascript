8036d200449f9168002bac627b0d77e2359851ea
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-09-27 19:32:24 +0530

    learning closoures

--- september2023/week5/day_3/practise.js
+++ september2023/week5/day_3/practise.js
@@ -1,3 +1,22 @@
-let sujit ="sujit"
+// let sujit ="sujit"
 
-console.log(sujit.slice(0,-1))
+// console.log(sujit.slice(0,-1))
+
+// function a(){
+//     let e=2
+//     function b(){
+//         console.log(e)
+//     }
+//     e=9
+//     b()
+// }
+
+// a()
+
+function lextureenvironment(){
+    console.log(x)
+}
+let x=10
+lextureenvironment()
+
+
