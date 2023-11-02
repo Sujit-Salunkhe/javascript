b0ee097c861fa98a57d44c91962ae6a1bea81cea
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-11-02 09:07:13 +0530

    learning objects and constructers

---
+++ November2023/week_1/day_4/objects.js
@@ -0,0 +1,16 @@
+myobj={
+    Name:"sujit",
+    lastName:"salunkhe",
+}
+
+// console.log(myobj['Name'])
+function createremplyee (firstname,lastName,salary){
+    
+    myobj.firstname=firstname,
+    myobj.lastName=lastName,
+    myobj.salary=salary
+    
+}
+
+let emp1= new createremplyee('sujit','salunkhe',20000);
+console.log(emp1)
