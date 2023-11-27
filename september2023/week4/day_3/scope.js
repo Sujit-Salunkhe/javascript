bbef0291edd4ec85d203e2b8f5ec6ae6d41e635d
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-11-27 20:37:14 +0530

    making new function button with adding two value

--- september2023/week4/day_3/scope.js
+++ september2023/week4/day_3/scope.js
@@ -3,7 +3,7 @@
 //     function getFullName() {
 //       return firstName + " " + lastName;
 //     }
-  
+
 //     alert( "Hello, " + getFullName() );
 //     alert( "Bye, " + getFullName() );
 //   }
@@ -12,13 +12,37 @@
 // // console.log("sujit is king")
 // {var sujit= "sujitsfd"}
 // alert(sujit)
-function a(){
-    b=9
-    c()
-    function c (){
-    console.log(b)
-    }
-}
-let b=10
-a()
+// function a(){
+//     b=9
+//     c()
+//     function c (){
+//     console.log(b)
+//     }
+// }
+// let b=10
+// a()
 
+
+
+
+let button = document.getElementById("Click");
+let valuetext = document.getElementById("valuetext")
+let flag = !true;
+let value = 0;
+button.addEventListener("click", () => {
+  if(flag === true){
+    flag = false
+  }
+  else {
+    flag = true
+  }
+  if (flag) {
+    value += 1;
+    console.log(value)
+  } else {
+    value += 5;
+    console.log(value);
+  }
+  valuetext.innerText = value
+  
+});
