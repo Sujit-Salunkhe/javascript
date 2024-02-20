cd8aa67cdaf0ae59d127068312baaf2923c05fda
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-02-20 16:27:15 +0530

    debug errror

--- November2023/week_1/day_4/objects.js
+++ November2023/week_1/day_4/objects.js
@@ -1,25 +1,34 @@
-myobj={
-    Name:"sujit",
-    lastName:"salunkhe",
-}
+// myobj={
+//     Name:"sujit",
+//     lastName:"salunkhe",
+// }
 
-// console.log(myobj['Name'])
-function createremplyee (firstname,lastName,salary){
+// // console.log(myobj['Name'])
+// function createremplyee (firstname,lastName,salary){
     
-    this.firstname=firstname,
-    this.lastName=lastName,
-    this.salary=salary
+//     this.firstname=firstname,
+//     this.lastName=lastName,
+//     this.salary=salary
     
-}
+// }
 
-let emp1= new createremplyee('sujit','salunkhe',20000);
-console.log(emp1)
+// let emp1= new createremplyee('sujit','salunkhe',20000);
+// console.log(emp1)
 
-function Car(speed,price,categoty){
-    this.speed=speed,
-    this.price=price,
-    this.categoty=categoty
-}
+// function Car(speed,price,categoty){
+//     this.speed=speed,
+//     this.price=price,
+//     this.categoty=categoty
+// }
 
-let wagonor = new car(100,500000,"small")
-console.log(wagonor)
+// let wagonor = new car(100,500000,"small")
+// console.log(wagonor)
+
+// let date = new Date('12/4/2020')
+// console.log(date)
+// // const date2 = 12/4/2027
+// let day  = date.getDay()
+// const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// console.log(daysOfWeek[day])
+// class Person:
+// console.log(23 & 7)
